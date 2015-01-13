fad4be125192c0b2f653448c7589fefe09dafbf2
Oleg Yashchuk <oazoer@gmail.com>
2015-01-13 08:38:48 +0300

    Add repository test

---
+++ spec/lib/proxy_enforce/proxy_repository_spec.rb
@@ -0,0 +1,47 @@
+require "spec_helper"
+
+RSpec.describe ProxyEnforce::ProxyRepository do
+  let(:klass) { described_class }
+  let(:repository) { klass.new }
+
+  describe "#initialize" do
+    it "defaults" do
+      expect_any_instance_of(klass).to receive(:reset!)
+      klass.new
+    end
+  end
+
+  describe "#reset!" do
+    before(:each) { repository.proxies << :foo }
+
+    it "reset to defauls" do
+      expect{repository.reset!}.to change{repository.proxies}.to([])
+    end
+  end
+
+  describe "#fetch" do
+    it "reset to defaults" do
+      expect(repository).to receive(:fetch)
+      repository.fetch
+    end
+
+    it "fetching test"
+  end
+
+  describe "#pull" do
+    let(:args) { {host: "1.1.1.1", port: 2020} }
+    let(:proxy) { ProxyEnforce::Proxy.new(args) }
+    let(:registry) { ProxyEnforce::ProxyRegistry.instance }
+
+    before(:each) do
+      expect(repository).to receive(:fetch) do
+        repository.proxies = [args]
+      end
+    end
+
+    it do
+      expect{repository.pull}.to change{registry.proxies.count}.by(1)
+      expect(registry.proxies.first.eql?(proxy)).to be true
+    end
+  end
+end
